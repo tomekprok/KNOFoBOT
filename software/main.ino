fd40e2189808610172548fc6495617dc9e92c899
t-prokop <t.prokop@student.uw.edu.pl>
2026-05-23 23:29:40 +0200

    renaming code, adding header file for pin definitions

--- software/main.ino
+++ software/main.ino
@@ -1,17 +1,4 @@
-const int PWM_MAX = 255;
-
-const int silnikLewy_PWM = 5;       // enA
-const int silnikPrawy_PWM = 10;     // enB
-
-const int silnikLewy_PRZOD = 6;     // in1
-const int silnikLewy_TYL = 7;       // in2
-
-const int silnikPrawy_PRZOD = 8;    // in3
-const int silnikPrawy_TYL = 9;      // in4
-
-const int sensorNadajnik = 12;      // trigPin
-const int sensorOdbiornik = 11;     // echoPin
-
+#include <pins.h>
 
 void set_pin_modes() {
   pinMode(silnikLewy_PWM, OUTPUT);
